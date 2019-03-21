e8ab543d439a6327fd51bb2e6e2b5df3bccb927c
zanderchase <zanderchase@gmail.com>
2019-03-21 13:01:36 -0400

    Add files via upload

---
+++ templates/map.js
@@ -0,0 +1,272 @@
+const map = L.map('map').setView([-25, 290], 3);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
+  maxZoom: 18
+}).addTo(map);
+
+// Adding Voyager Labels
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  zIndex: 10
+}).addTo(map);
+
+var client = new carto.Client({
+  apiKey: 'f1dfb75f268561a2d0eeabad6631f9bcf144996b',
+  username: 'zanderchase'
+});
+
+const LACountriesDataset = new carto.source.Dataset(`
+  samerica_adm0
+`);
+const LACountriesStyle = new carto.style.CartoCSS(`
+  #layer {
+  polygon-fill: #162945;
+    polygon-opacity: 0.5;
+    ::outline {
+      line-width: 1;
+      line-color: #FFFFFF;
+      line-opacity: 0.5;
+    }
+  }
+`);
+const LACountriesPoly = new carto.layer.Layer(LACountriesDataset, LACountriesStyle);
+
+const LAWordsSource = new carto.source.SQL(`
+  SELECT *
+    FROM test_scraper2_clean
+    WHERE text like '% boludo %'
+`);
+
+const LAWordsStyle = new carto.style.CartoCSS(`
+  #layer {
+    marker-width: 6;
+    marker-fill: #FF583E;
+    marker-fill-opacity: 0.4;
+    marker-line-width: 0.5;
+    marker-line-color: #FFFFFF;
+    marker-line-opacity: 0.4;
+    marker-type: ellipse;
+    marker-allow-overlap: true;
+    marker-comp-op: src-atop;
+
+  }
+`);
+
+
+
+
+
+//marker-comp-op: src-atop;
+const LAWords = new carto.layer.Layer(LAWordsSource, LAWordsStyle, {
+  featureOverColumns: ['user_name']
+});
+
+
+client.addLayers([LACountriesPoly, LAWords]);
+client.getLeafletLayer().addTo(map);
+
+
+const popup = L.popup({ closeButton: false });
+  LAWords.on(carto.layer.events.FEATURE_OVER, featureEvent => {
+    popup.setLatLng(featureEvent.latLng);
+    if (!popup.isOpen()) {
+      popup.setContent(featureEvent.data.user_name);
+      popup.openOn(map);
+    }
+  });
+
+LAWords.on(carto.layer.events.FEATURE_OUT, featureEvent => {
+  popup.removeFrom(map);
+});
+//console.log('dataview')
+const wordDataview = new
+carto.dataview.Category(LACountriesDataset, 'adm0_a3', {
+  limit: 16
+});
+wordDataview.on('dataChanged', data => {
+  const countryNames = data.categories.map(category => category.name).sort();
+  //console.log(countryNames);
+  console.log('dv1')
+  refreshWordsWidget(countryNames);
+});
+
+function refreshWordsWidget(adminNames) {
+  //console.log('refresh')
+  const widgetDom = document.querySelector('#wordWidget');
+  const wordsDom = widgetDom.querySelector('.js-words');
+
+  const widgetDom2 = document.querySelector('#countriesWidget');
+  const wordsDom2 = widgetDom2.querySelector('.js-countries');
+
+  const widgetDom3 = document.querySelector('#selectorWidget');
+  const wordsDom3 = widgetDom3.querySelector('.js-selector');
+
+  wordsDom.onchange = event => {
+    var e = document.getElementById("countriesWidget");
+
+    //console.log(wordsDom2.options[wordsDom2.selectedIndex].value)
+    var country = wordsDom2.options[wordsDom2.selectedIndex].value
+    const admin = event.target.value;
+    filterPopulatedPlacesByWord(admin, country);
+  };
+
+  wordsDom2.onchange = event => {
+    //console.log('change')
+    var word = wordsDom.options[wordsDom.selectedIndex].value
+    const admin = event.target.value;
+
+    highlightCountry(admin);
+    filterCountries(admin, word);
+  };
+
+  wordsDom3.onchange = event => {
+    //console.log('change')
+    const admin = event.target.value;
+    LayerActions(admin);
+  };
+
+  adminNames.forEach(admin => {
+    //console.log(admin);
+    const option = document.createElement('option');
+    option.innerHTML = admin;
+    option.value = admin;
+    wordsDom2.appendChild(option);
+  });
+}
+
+function LayerActions(admin) {
+  console.log(admin)
+  if (admin == 'dots'){
+      LAWordsStyle.setContent(`
+        #layer {
+          marker-width: 6;
+          marker-fill: #FF583E;
+          marker-fill-opacity: 0.4;
+          marker-line-width: 0.5;
+          marker-line-color: #FFFFFF;
+          marker-line-opacity: 0.4;
+          marker-type: ellipse;
+          marker-allow-overlap: true;
+          marker-comp-op: src-atop;
+
+        }
+      `);
+    }
+  else if (admin == 'heatmap') {
+      LAWordsStyle.setContent(`
+        #layer {
+          image-filters: colorize-alpha(blue, cyan, #008000, yellow , orange, red);
+          marker-file: url(http://s3.amazonaws.com/com.cartodb.assets.static/alphamarker.png);
+
+          marker-width: 15;
+          marker-width: 6;
+          marker-fill: #FF583E;
+          marker-fill-opacity: 0.03;
+          marker-line-width: 0.5;
+          marker-line-color: #FFFFFF;
+          marker-line-opacity: 0.03;
+          marker-type: ellipse;
+          marker-allow-overlap: true;
+
+
+        }
+      `);
+    }
+}
+function highlightCountry(admin) {
+  //console.log(admin);
+  let cartoCSS = `
+    #layer {
+      polygon-fill: #162945;
+      polygon-opacity: 0.5;
+      ::outline {
+        line-width: 1;
+        line-color: #FFFFFF;
+        line-opacity: 0.5;
+      }
+    }
+  `;
+  if (admin) {
+    cartoCSS = `
+      ${cartoCSS}
+      #layer[adm0_a3!='${admin}'] {
+        polygon-fill: #CDCDCD;
+      }
+    `;
+  }
+  LACountriesStyle.setContent(cartoCSS);
+}
+
+function filterCountries(admin, word) {
+  console.log(word)
+  let query = `
+    SELECT *
+      FROM test_scraper2_clean
+      WHERE country IN (SELECT adm0_a3 FROM samerica_adm0) AND text like '% ${word} %'
+  `;
+  if (admin) {
+    query = `
+      SELECT *
+        FROM test_scraper2_clean
+        WHERE country='${admin}' AND text like '% ${word} %'
+    `;
+  }
+  LAWordsSource.setQuery(query);
+}
+
+// Fill in the list of words
+const widgetDom = document.querySelector('#wordWidget');
+const wordsDom = widgetDom.querySelector('.js-words');
+const wordNames = ['popcorn', 'canchita','pochoclo', 'crispetas', 'pop', 'pororo', 'pororó','almacén','acera', 'andén', 'badén', 'calzada', 'contén', 'escarpa', 'vereda']
+wordNames.forEach(admin => {
+  const option = document.createElement('option');
+  option.innerHTML = admin;
+  option.value = admin;
+  wordsDom.appendChild(option);
+});
+
+function filterPopulatedPlacesByWord(admin, country) {
+  let query = `
+    SELECT *
+      FROM test_scraper2_clean
+      WHERE text IS NOT NULL AND country IN (SELECT adm0_a3 FROM samerica_adm0)
+  `;
+  if (admin && country != '') {
+    console.log('1')
+    query = `
+      SELECT *
+        FROM test_scraper2_clean
+        WHERE text like '% ${admin} %' AND country = '${country}'
+    `;
+  }
+  else if (admin) {
+    console.log('2')
+    query = `
+      SELECT *
+        FROM test_scraper2_clean
+        WHERE text like '% ${admin} %'
+    `;
+  }
+
+  LAWordsSource.setQuery(query);
+}
+
+
+//how many words
+const averagePopulation = new carto.dataview.Formula(LAWordsSource, 'one', {
+  operation: carto.operation.AVG
+});
+
+averagePopulation.on('dataChanged', data => {
+  refreshAveragePopulationWidget(data.result);
+});
+
+function refreshAveragePopulationWidget(avgPopulation) {
+  console.log('here')
+  console.log(avgPopulation)
+  const widgetDom = document.querySelector('#avgPopulationWidget');
+  const averagePopulationDom = widgetDom.querySelector('.js-average-population');
+  averagePopulationDom.innerText = Math.floor(avgPopulation);
+}
+client.addDataview(averagePopulation);
+client.addDataview(wordDataview);
---
+++ templates/quiz.js
@@ -0,0 +1,192 @@
+var answers = [];
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, chE;
+var questions = [
+  ["Cómo se llama un modo de transportación con cuatro ruedas?", "Coche", "Auto", "Carro", ""],
+  ["Cómo se llama algo que lleva cuando hace frio?", "Chaqueta", "Campera", "Saco", "Casca"],
+  ["Cómo se llama el aparato donde uno pone comida?", "Nevera", "Heladera", "Refrigerador", "Refrigeradora"],
+  ["Cómo se llama al postre que se come en fiestas?", "Pastel", "Queque", "Ponqué", "Torta"],
+  ["Cómo se llama una enfermedad con estornudos y secreción nasal?", "Gripa", "Resfrío", "Resfriado", "Gripe"]
+];
+
+var rioplat_questions = [
+  ["Cómo se llama granos de maíz inflados?", "Pochoclo", "Pop", "Pororó", "Canchita"],
+];
+var norte_questions = [
+  ["Cómo se llama granos de maíz inflados?", "Cotufas", "Crispetas", "Maíz pira", "Gallitos"],
+];
+var centro_questions = [
+  ["Cómo se llama granos de maíz inflados?", "Canchita", "Pipoca", "Pororó", "Canguil"]
+];
+var chile_questions = [
+  ["Chile, no?", "Sí", "No", "Es posible", ""]
+];
+
+questions_dict = {
+  'Argentina': rioplat_questions,
+  'Paraguay': rioplat_questions,
+  'Uruguay': rioplat_questions,
+  'Colombia': norte_questions,
+  'Venezuela': norte_questions,
+  'Peru': centro_questions,
+  'Ecuador': centro_questions,
+  'Bolivia': centro_questions,
+  'Chile': chile_questions,
+};
+function _(x) {
+  return document.getElementById(x);
+}
+function calculateRegion(answers){
+  var argentina = [{A:0, B:3, C:0}, {A:4, B:0, C:0, D:0}, {A:0, B:3, C:0, D:0}, {A:0, B:0, C:0, D:1},
+     {A:0, B:4, C:0, D:2}];
+
+  var paraguay = [{A:0, B:3, C:0}, {A:4, B:0, C:0, D:0}, {A:0, B:3, C:0, D:0}, {A:0, B:0, C:0, D:1},
+     {A:0, B:0, C:0, D:3}];
+  var uruguay = [{A:0, B:3, C:0}, {A:4, B:0, C:0, D:0}, {A:0, B:3, C:0, D:0}, {A:0, B:0, C:0, D:1},
+     {A:0, B:3, C:0, D:3}];
+
+  var chile = [{A:0, B:3, C:0}, {A:0, B:0, C:2, D:2}, {A:0, B:0, C:3, D:0}, {A:0, B:0, C:0, D:1},
+     {A:0, B:3, C:2, D:0}];
+
+  var bolivia = [{A:0, B:0, C:3}, {A:0, B:0, C:2, D:2}, {A:0, B:3, C:0, D:0}, {A:0, B:4, C:0, D:0},
+     {A:0, B:4, C:2, D:0}];
+  var ecuador = [{A:0, B:0, C:3}, {A:0, B:0, C:2, D:2}, {A:2, B:0, C:2, D:4}, {A:0, B:0, C:0, D:1},
+     {A:0, B:0, C:0, D:3}];
+  var peru = [{A:0, B:0, C:3}, {A:0, B:4, C:0, D:0}, {A:0, B:0, C:3, D:0}, {A:0, B:4, C:0, D:1},
+     {A:0, B:0, C:1, D:3}];
+
+  var venezuela = [{A:0, B:0, C:3}, {A:0, B:0, C:2, D:2}, {A:3, B:0, C:0, D:0}, {A:0, B:0, C:0, D:1},
+     {A:0, B:0, C:0, D:3}];
+  var colombia = [{A:0, B:0, C:3}, {A:0, B:0, C:2, D:2}, {A:3, B:0, C:0, D:0}, {A:0, B:0, C:4, D:1},
+     {A:4, B:0, C:0, D:1}];
+
+  var my_list = [argentina, paraguay, uruguay, chile, bolivia, ecuador, peru, venezuela, colombia];
+  var my_names = ['Argentina', 'Paraguay', 'Uruguay', 'Chile', 'Bolivia', 'Ecuador', 'Peru', 'Venezuela', 'Colombia'];
+
+  var greatest = 0;
+
+  for (i=0; i < my_list.length; i++) {
+    var counter = 0
+    var region = my_list[i];
+    for (j=0; j < 5; j++) {
+      console.log(region[j])
+      console.log(answers)
+      counter += region[j][answers[j]]
+    }
+    if (counter >= greatest){
+      greatest = counter
+      return_number = i
+    }
+  }
+  return (my_names[return_number])
+}
+
+function calculateCityARG(answers){
+  var buenos_aires = [{A:4, B:0, C:0, D:0}];
+  var cordoba = [{A:3, B:0, C:0, D:0}];
+  var mendoza = [{A:3, B:0, C:0, D:0}];
+
+
+  var my_list = [buenos_aires, cordoba, mendoza];
+  var my_names = ['Buenos Aires', 'Córdoba', 'Mendoza'];
+
+  var greatest = 0;
+
+  for (i=0; i < my_list.length; i++) {
+    var counter = 0
+    var region = my_list[i];
+    for (j=0; j < region.length; j++) {
+      console.log(region[j])
+      console.log(answers)
+      counter += region[j][answers[j]]
+    }
+    if (counter >= greatest){
+      greatest = counter
+      return_number = i
+    }
+  }
+  return (my_names[return_number])
+}
+
+function renderQuestion(quests, poss){
+  test = _("test");
+  _("test_status").innerHTML = "Question " + (pos + 1) + " of " + questions.length;
+  //console.log('here')
+  question = quests[poss][0];
+  //console.log(question)
+  chA = quests[poss][1];
+  chB = quests[poss][2];
+  chC = quests[poss][3];
+  chD = quests[poss][4];
+  test.innerHTML = "<h3>" + (question) + "</h3>";
+  test.innerHTML += "<input type='radio' name='choices' value='A'> " + chA + "<br>";
+  test.innerHTML += "<input type='radio' name='choices' value='B'> " + chB + "<br>";
+  test.innerHTML += "<input type='radio' name='choices' value='C'> " + chC + "<br>";
+  if (chD != ""){
+    test.innerHTML += "<input type='radio' name='choices' value='D'> " + chD + "<br>";
+  }
+  test.innerHTML += "<br><button onclick='checkAnswer()'> Submit Answer </button>"
+  //console.log('finished')
+}
+
+function examineRegionQuestion(poss, region){
+  test = _("test");
+  _("test_status").innerHTML = "Question " + (pos + 1) + " of " + 6;
+  //console.log('here')
+
+  new_pos = poss-5
+  quests = questions_dict[region]
+  question = quests[new_pos][0];
+  //console.log(question)
+  chA = quests[new_pos][1];
+  chB = quests[new_pos][2];
+  chC = quests[new_pos][3];
+  chD = quests[new_pos][4];
+  test.innerHTML = "<h3>" + (question) + "</h3>";
+  test.innerHTML += "<input type='radio' name='choices' value='A'> " + chA + "<br>";
+  test.innerHTML += "<input type='radio' name='choices' value='B'> " + chB + "<br>";
+  test.innerHTML += "<input type='radio' name='choices' value='C'> " + chC + "<br>";
+  if (chD != ""){
+    test.innerHTML += "<input type='radio' name='choices' value='D'> " + chD + "<br>";
+  }
+  test.innerHTML += "<br><button onclick='checkAnswer()'> Submit Answer </button>"
+  //console.log('finished')
+}
+
+
+function checkAnswer() {
+  //alert("Ok, will do")
+  choices = document.getElementsByName("choices");
+  for (var i = 0; i < choices.length; i++){
+    if (choices[i].checked) {
+      //console.log('checked')
+      choice = choices[i].value;
+      console.log('yooo')
+      console.log(choice)
+    }
+  }
+  answers.push(choice)
+  console.log('answers')
+  console.log(answers)
+  pos ++;
+  if(pos == 5){
+    var region = calculateRegion(answers);
+    alert('quiz over' + region)
+    examineRegionQuestion(pos, region);
+  }
+  else if (pos == 6){
+    console.log('HERE')
+    var region = calculateRegion(answers);
+    console.log(region)
+    if(region == 'Argentina') {
+      console.log(answers.slice(5,))
+      var city = calculateCityARG(answers.slice(5,));
+      alert('quiz actually over' + city)
+    }
+    //alert('quiz over' + region)
+    //examineRegionQuestion();
+  }
+  else {
+    renderQuestion(questions, pos);
+  }
+}
+window.addEventListener("load", function(){renderQuestion(questions, pos)}, false);
